af02de7cea71c410906388378b87d481d30f41ba
Jerecowong <haiou77@yahoo.com>
2015-03-01 15:19:16 -0800

    Add code to save coursera tokenizer and vectors

--- code/CourseraTokenizer.py
+++ code/CourseraTokenizer.py
@@ -2,6 +2,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 from sklearn.metrics.pairwise import linear_kernel
 import numpy as np
+import cPickle as pkl
 
 
 def get_top_courses(lst, n, courses):
@@ -13,7 +14,7 @@ def get_top_courses(lst, n, courses):
     Given a list of cosine similarities, find the indices with the highest n values.
     Return the courses for each of these indices.
     '''
-    return [(courses[i], lst(i))for i in np.argsort(lst)[-1:-n - 1:-1]]
+    return [(courses[i], lst[i])for i in np.argsort(lst)[-1:-n - 1:-1]]
 
 
 class CourseraTokenizer(object):
@@ -27,10 +28,10 @@ class CourseraTokenizer(object):
         - df:
         OUTPUT: None
         '''
-        ngram_range = (1, 1)
-        max_features = 1000
-        self.vectorizer = TfidfVectorizer(stop_words='english', gram_range=gram_range,
-                                    max_features=max_features)
+        self.ngram_range = (1, 1)
+        self.max_features = 1000
+        self.vectorizer = TfidfVectorizer(stop_words='english', ngram_range=self.ngram_range,
+                                    max_features=self.max_features)
         self.vectors = None
         self.df = None
 
@@ -39,7 +40,7 @@ class CourseraTokenizer(object):
 
     def set_df(self, file_name):
         self.df = pd.read_json(file_name)
-        self.df['description'] = df['name'] + ' ' + df['shortDescription']
+        self.df['description'] = self.df['name'] + ' ' + self.df['shortDescription']
 
     def get_vectors(self):
         return self.vectors
@@ -64,8 +65,8 @@ if __name__ == '__main__':
     coursera_tokenizer = CourseraTokenizer()
     coursera_tokenizer.set_df('../data/courses_desc.json')
     coursera_tokenizer.set_vectors()
-    cousera_vectorizer = coursera_tokenizer.get_vectorizer()
-    cousera_vectors = coursera_tokenizer.get_vectors()
+    coursera_vectorizer = coursera_tokenizer.get_vectorizer()
+    coursera_vectors = coursera_tokenizer.get_vectors()
     coursera_courses = coursera_tokenizer.get_course_shortnames()
     requirements = ["A passion for making sense of lots of unstructured data",
                     "Experience with NLP, classification, graph mining, and/or recommender systems",
@@ -77,9 +78,15 @@ if __name__ == '__main__':
                     "Enthusiasm for working in a fun, dynamic startup environment",
                     "You like to shop online and you don't mind getting reimbursed to buy more stuff!",
                     "You can code in the presence of flying ping pong balls."]
-    requirements_vectors = cousera_vectorizer.transform(requirements)
-    cosine_similarities = linear_kernel(requirements_vectors, cousera_vectors)
+    requirements_vectors = coursera_vectorizer.transform(requirements)
+    cosine_similarities = linear_kernel(requirements_vectors, coursera_vectors)
     for i, requirement in enumerate(requirements):
         print requirement
         print get_top_courses(cosine_similarities[i], 3, coursera_courses)
         print
+    #save the coursera_tokenizer and coursera_vectors
+    with open('../data/coursera_tokenizer.pkl', 'wb') as handle:
+        pkl.dump(coursera_tokenizer, handle)
+    with open('../data/coursera_vectors.pkl', 'wb') as handle:
+        pkl.dump(coursera_vectors, handle)
+
